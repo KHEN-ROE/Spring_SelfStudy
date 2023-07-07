eb47bd75033510dc5c1fe42ddbd30b2709b4caf5
KEN <shdnrnjs@gmail.com>
2023-07-07 17:55:57 +0900

    섹션5 들을 차례

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,48 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) { // 외부에서 넣어준다. 이런 것을 의존관계 주입이라고 함
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원가입
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복회원은 안 된다.
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원");
+                });
+    }
+
+    // 전체회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+}

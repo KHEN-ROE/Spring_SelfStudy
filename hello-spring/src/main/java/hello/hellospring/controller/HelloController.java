20965188b996b687491ba11037b4ff1ff81aab08
KEN <shdnrnjs@gmail.com>
2023-07-07 14:55:54 +0900

    섹션3 - 회원서비스 개발 들을 차례

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,37 @@ public class HelloController {
         // model에 key, value 담아서 hello.html 뷰 리턴
         model.addAttribute("data", "hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-String")
+    @ResponseBody // http(헤더 -바디)의 바디 부분을 직접 리턴하겠다는 뜻. view 없이 문자 그대로 리턴
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // 객체는 JSON방식으로 데이터를 만들어서 리턴
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }

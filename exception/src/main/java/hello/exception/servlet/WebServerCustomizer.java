934682cf4555673b51a7ab5c71c3fd3e5b419922
KEN <shdnrnjs@gmail.com>
2023-08-02 16:33:20 +0900

    API 예외처리 남음

---
+++ exception/src/main/java/hello/exception/servlet/WebServerCustomizer.java
@@ -0,0 +1,22 @@
+package hello.exception.servlet;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+// 오류 페이지 커스터마이징
+//@Component // 컴포넌트로 등록해야 동작함
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+
+        // 스프링부트가 제공하는 오류페이지
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
+}
